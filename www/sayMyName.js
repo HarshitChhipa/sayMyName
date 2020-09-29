4b7aff4e98bc49cf1082b3ea3cb6630913ea475c
Harshit Chhipa <harshit.kankerwal@milkbasket.com>
2020-09-29 22:22:28 +0530

    Adding js code for plugin

--- www/sayMyName.js
+++ www/sayMyName.js
@@ -1,5 +1,8 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function (arg0, success, error) {
-    exec(success, error, 'sayMyName', 'coolMethod', [arg0]);
+// exports.coolMethod = function (arg0, success, error) {
+//     exec(success, error, 'sayMyName', 'coolMethod', [arg0]);
+// };
+window.sayMyName = function(str, callback) {
+    cordova.exec(callback, function(err) { callback('Nothing to echo.');}, "SayMyName", "sayMyName", [str]);
 };
