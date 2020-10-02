e2d08409cd757611e998764cbb8d3482dbf20594
Harshit Chhipa <harshitkankerwal@gmail.com>
2020-10-02 23:40:54 +0530

    Formatted js file

--- www/sayMyName.js
+++ www/sayMyName.js
@@ -1,8 +1,16 @@
-var exec = require('cordova/exec');
+var exec = require("cordova/exec");
 
-exports.coolMethod = function (arg0, success, error) {
-    exec(success, error, 'sayMyName', 'coolMethod', [arg0]);
-};
-// window.sayMyName = function(str, callback) {
-//     cordova.exec(callback, function(err) { callback('Nothing to echo.');}, "SayMyName", "sayMyName", [str]);
+// exports.coolMethod = function (arg0, success, error) {
+//     exec(success, error, 'sayMyName', 'coolMethod', [arg0]);
 // };
+window.sayMyName = function (str, callback) {
+  exec(
+    callback,
+    function (err) {
+      callback("I broke somewhere while processing.");
+    },
+    "SayMyName",
+    "sayMyName",
+    [str]
+  );
+};
