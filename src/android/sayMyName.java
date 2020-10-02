864972eb4ce69c85449b8c5e7d4e52274bdc47fe
Harshit Chhipa <harshitkankerwal@gmail.com>
2020-10-03 00:41:26 +0530

    Did java side changes for sayMyName

--- src/android/sayMyName.java
+++ src/android/sayMyName.java
@@ -1,8 +1,9 @@
-package cordova-plugin-saymyname;
+package cordova.plugin.saymyname;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
-
+import org.apache.CordovaInterface;
+import org.apache.CordovaWebView;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -13,20 +14,26 @@ import org.json.JSONObject;
 public class sayMyName extends CordovaPlugin {
 
     @Override
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+        super.initialize(cordova, webView);
+        // your init code here
+    }
+
+    @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("coolMethod")) {
+        if (action.equals("sayMyName")) {
             String message = args.getString(0);
-            this.coolMethod(message, callbackContext);
+            this.sayMyNameMethod(message, callbackContext);
             return true;
         }
         return false;
     }
 
-    private void coolMethod(String message, CallbackContext callbackContext) {
+    private void sayMyNameMethod(String message, CallbackContext callbackContext) {
         if (message != null && message.length() > 0) {
-            callbackContext.success(message);
+            callbackContext.success('Harshit Chhips is My Name');
         } else {
-            callbackContext.error("Expected one non-empty string argument.");
+            callbackContext.error("Comon man Give me something at least.");
         }
     }
 }
--- www/sayMyName.js
+++ www/sayMyName.js
@@ -3,7 +3,7 @@ var exec = require("cordova/exec");
 // exports.coolMethod = function (arg0, success, error) {
 //     exec(success, error, 'sayMyName', 'coolMethod', [arg0]);
 // };
-window.sayMyName = function (str, callback) {
+var sayMyName = function (str, callback) {
   exec(
     callback,
     function (err) {
@@ -14,3 +14,5 @@ window.sayMyName = function (str, callback) {
     [str]
   );
 };
+
+module.exports = sayMyName;
