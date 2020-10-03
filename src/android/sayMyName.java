0480daffa8801cbf8dd1458024ec3f7193c6b216
Harshit Chhipa <harshitkankerwal@gmail.com>
2020-10-03 13:54:30 +0530

    exported the js variable trying to run the Plugin its my 8th attempty lets see how it goes I m not getting what I am missing

--- src/android/sayMyName.java
+++ src/android/sayMyName.java
@@ -2,6 +2,9 @@ package cordova.plugin.saymyname;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaWebView;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -12,21 +15,26 @@ import org.json.JSONObject;
 public class sayMyName extends CordovaPlugin {
 
 
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+        super.initialize(cordova, webView);
+    }
+
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if (action.equals("sayMyName")) {
             String message = args.getString(0);
-            this.sayMyNameMethod(message, callbackContext);
+            cordova.getActivity().runOnUiThread(new Runnable() {
+                public void run() {
+                    // callbackContext.success(); // Thread-safe.
+                    if (message != null && message.length() > 0) {
+                        callbackContext.success("Chhipa....Harshit Chhipa");
+                    } else {
+                        callbackContext.error("Common man Give me something at least.");
+                    }
+                }
+            });
             return true;
         }
         return false;
-    }
-
-    private void sayMyNameMethod(String message, CallbackContext callbackContext) {
-        if (message != null && message.length() > 0) {
-            callbackContext.success("Harshit Chhips is My Name");
-        } else {
-            callbackContext.error("Comon man Give me something at least.");
-        }
     }
 }
--- www/sayMyName.js
+++ www/sayMyName.js
@@ -1,16 +1,7 @@
-var exec = require("cordova/exec");
+var exec = require('cordova/exec');
 
-// exports.coolMethod = function (arg0, success, error) {
-//     exec(success, error, 'sayMyName', 'coolMethod', [arg0]);
-// };
-window.sayMyName = function (str, callback) {
-  cordova.exec(
-    callback,
-    function (err) {
-      callback("I broke somewhere while processing.");
-    },
-    "SayMyName",
-    "sayMyName",
-    [str]
-  );
-};
+module.exports = {
+  match: function(stringPassed, callback) {
+      exec(callback, callback, "SayMyName", "sayMyName", [stringPassed]);
+  },
+}
