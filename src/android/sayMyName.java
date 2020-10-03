0db3d47ff5d85ad5a15eb8e6b836278b670a078a
Harshit Chhipa <harshitkankerwal@gmail.com>
2020-10-03 14:20:25 +0530

    Kya he pta ab bhi chalega ya nhi

--- src/android/sayMyName.java
+++ src/android/sayMyName.java
@@ -8,24 +8,30 @@ import org.apache.cordova.CordovaWebView;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import android.util.Log;
+import android.util.Log;
 
 /**
  * This class echoes a string called from JavaScript.
  */
 public class sayMyName extends CordovaPlugin {
+    private final static String LOG_TAG = "SAY_MY_NAME";
+    private static CordovaInterface cordova = null;
+    private CallbackContext cordovaCallBack;
 
-
-    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+    public void initialize(CordovaInterface initCordova, CordovaWebView webView) {
+        Log.d(LOG_TAG, "initialized");
+        cordova = initCordova;
         super.initialize(cordova, webView);
     }
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        Log.d(LOG_TAG, "executed");
         if (action.equals("sayMyName")) {
             String message = args.getString(0);
             cordova.getActivity().runOnUiThread(new Runnable() {
                 public void run() {
-                    // callbackContext.success(); // Thread-safe.
                     if (message != null && message.length() > 0) {
                         callbackContext.success("Chhipa....Harshit Chhipa");
                     } else {
