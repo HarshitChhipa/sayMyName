73ad705168b1bfb2233d9f6bca26c176c8dd3d38
Harshit Chhipa <harshitkankerwal@gmail.com>
2020-10-03 12:16:42 +0530

    correctd the package

--- src/android/sayMyName.java
+++ src/android/sayMyName.java
@@ -2,8 +2,6 @@ package cordova.plugin.saymyname;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
-import org.apache.cordova.CordovaInterface;
-import org.apache.cordova.CordovaWebView;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -13,11 +11,6 @@ import org.json.JSONObject;
  */
 public class sayMyName extends CordovaPlugin {
 
-    @Override
-    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
-        super.initialize(cordova, webView);
-        // your init code here
-    }
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
